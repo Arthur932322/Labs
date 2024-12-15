174f12c83a9f569a1c89813b37068072a285adc0
Arthur932322 <artur.aidekov@gmail.com>
2024-12-15 17:54:28 +0700

    Реализован пункт 2 лабораторной 4 АиСД

--- Algos_Laba4/Algos_Laba4/Algos_Laba4.cpp
+++ Algos_Laba4/Algos_Laba4/Algos_Laba4.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
 #include "Header.h"
 
+std::vector<int> generateRandomArray(int size, int minValue, int maxValue) {
+    std::vector<int> arr(size);
+    for (int& num : arr) {
+        num = minValue + rand() % (maxValue - minValue + 1);
+    }
+    return arr;
+}
+
+double measureSortTime(std::vector<int>& arr, int runs) {
+    double totalTime = 0.0;
+    for (int i = 0; i < runs; ++i) {
+        std::vector<int> tempArr = arr;
+        auto start = std::chrono::high_resolution_clock::now();
+        heapSort(tempArr);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+        totalTime += duration.count();
+    }
+    return totalTime / runs;
+}
+
 int main() {
+
     setlocale(LC_ALL, "Russian");
     std::vector<int> arr = { 10, 3, 76, 34, 23, 32 };
 
@@ -27,5 +52,27 @@ int main() {
         std::cout << "ћассив не отсортирован." << std::endl;
     }
 
+    srand(static_cast<unsigned>(time(0)));
+
+    std::vector<int> sizes = { 10000, 100000, 1000000 };
+    std::vector<std::pair<int, int>> ranges = { {-10, 10}, {-1000, 1000}, {-100000, 100000} };
+    const int runs = 3; 
+
+    for (int size : sizes) {
+        for (const auto& range : ranges) {
+            int minValue = range.first;
+            int maxValue = range.second;
+
+            std::cout << "“естирование массива размером " << size
+                << " с диапазоном значений [" << minValue << ", " << maxValue << "]\n";
+
+            std::vector<int> arr = generateRandomArray(size, minValue, maxValue);
+
+            double averageTime = measureSortTime(arr, runs);
+            std::cout << "—реднее врем€ сортировки: " << averageTime << " секунд\n";
+        }
+    }
+
+  
     return 0;
 }
