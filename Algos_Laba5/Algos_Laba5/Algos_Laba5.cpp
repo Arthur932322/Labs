7363a484937c1f99d72d2bd92f1066b94cd23afe
Arthur932322 <artur.aidekov@gmail.com>
2024-12-15 17:55:08 +0700

    Реализован пункт 2 лабораторной 5 АиСд

--- Algos_Laba5/Algos_Laba5/Algos_Laba5.cpp
+++ Algos_Laba5/Algos_Laba5/Algos_Laba5.cpp
@@ -1,6 +1,30 @@
 ﻿#include "Header.h"
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+
+std::vector<int> generateRandomArray(int size, int minValue, int maxValue) {
+    std::vector<int> arr(size);
+    for (int& num : arr) {
+        num = minValue + rand() % (maxValue - minValue + 1);
+    }
+    return arr;
+}
+
+double measureSortTime(std::vector<int>& arr, int runs) {
+    double totalTime = 0.0;
+    for (int i = 0; i < runs; ++i) {
+        std::vector<int> tempArr = arr; 
+        auto start = std::chrono::high_resolution_clock::now();
+        quickSort(tempArr, 0, tempArr.size() - 1);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+        totalTime += duration.count();
+    }
+    return totalTime / runs; 
+}
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -25,6 +49,27 @@ int main() {
     }
     else {
         std::cout << "Ошибка: массив не отсортирован." << std::endl;
+    }
+
+    srand(static_cast<unsigned>(time(0)));
+
+    std::vector<int> sizes = { 10000, 100000 };
+    std::vector<std::pair<int, int>> ranges = { {-10, 10}, {-1000, 1000}, {-100000, 100000} };
+    const int runs = 3;
+
+    for (int size : sizes) {
+        for (const auto& range : ranges) {
+            int minValue = range.first;
+            int maxValue = range.second;
+
+            std::cout << "Тестирование массива размером " << size
+                << " с диапазоном значений [" << minValue << ", " << maxValue << "]\n";
+
+            std::vector<int> arr = generateRandomArray(size, minValue, maxValue);
+
+            double averageTime = measureSortTime(arr, runs);
+            std::cout << "Среднее время сортировки: " << averageTime << " секунд\n";
+        }
     }
 
     return 0;
